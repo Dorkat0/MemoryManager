1083d7b3d5de3ea3cfcb6d2dca47e811e6f5dccf
Dorkat0 <14137285+Dorkat0@users.noreply.github.com>
2020-04-25 09:02:35 +0200

    empty project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Memory_Manager
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
